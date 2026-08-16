f644b40bf68a318732e3cdda3f35acd8cfa7768e
Osani <osani@bovedaarcana.local>
2026-08-16 17:18:59 +0000

    Fix: sitemap.xml.js tronaba el build porque no es una API route real, usar getServerSideProps

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,10 +1,18 @@
-export default function handler(req, res) {
+export async function getServerSideProps({ req, res }) {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || `https://${req.headers.host}`;
   const pages = ['', '/mis-pedidos', '/cuenta'];
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages.map(p => `  <url><loc>${baseUrl}${p}</loc></url>`).join('\n')}
 </urlset>`;
+
   res.setHeader('Content-Type', 'application/xml');
-  res.status(200).send(xml);
+  res.write(xml);
+  res.end();
+
+  return { props: {} };
+}
+
+export default function Sitemap() {
+  return null;
 }
